cc14166ff43285ac2a3ef0eba7d0231969055ca6
aleczhang <aleczhang@163.com>
2016-12-29 10:57:43 +0800

    通过vue-cli创建加入mint-ui

---
+++ src/components/Hello.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+  	<mt-navbar v-model="selected">
+  <mt-tab-item id="1">选项一</mt-tab-item>
+  <mt-tab-item id="2">选项二</mt-tab-item>
+  <mt-tab-item id="3">选项三</mt-tab-item>
+</mt-navbar>
+
+<!-- tab-container -->
+<mt-tab-container v-model="selected">
+  <mt-tab-container-item id="1">
+    <mt-cell v-for="n in 10" :title="'内容 ' + n" />
+  </mt-tab-container-item>
+  <mt-tab-container-item id="2">
+    <mt-cell v-for="n in 4" :title="'测试 ' + n" />
+  </mt-tab-container-item>
+  <mt-tab-container-item id="3">
+    <mt-cell v-for="n in 6" :title="'选项 ' + n" />
+  </mt-tab-container-item>
+</mt-tab-container>
+
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Navbar, TabItem, TabContainer, TabContainerItem, Cell } from 'mint-ui'
+
+Vue.component(Navbar.name, Navbar)
+Vue.component(TabItem.name, TabItem)
+Vue.component(TabContainer.name, TabContainer)
+Vue.component(TabContainerItem.name, TabContainerItem)
+Vue.component(Cell.name, Cell)
+export default {
+  name: 'hello',
+  data () {
+    return {
+      selected: '1'
+    }
+  },
+  components: {
+    'mt-navbar': Navbar,
+    'mt-tab-item': TabItem,
+    'mt-tab-container': TabContainer,
+    'mt-tab-container-item': TabContainerItem,
+    'mt-cell': Cell
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
